ed7e9040f12aaaeb95b058d3ddfdacebb10035e0
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-02-19 12:47:46 +0530

    Time: 248 ms (5.37%), Space: 16.2 MB (5.39%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    ListNode* head = NULL;
+private:
+    void insertAtEnd(ListNode* &head, int val){
+        ListNode* temp = new ListNode;
+        temp->val = val;
+        if(head == NULL){
+            head = temp;
+            temp->next = NULL;
+            return;
+        }
+        ListNode* curr = head;
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = temp;
+        temp->next = NULL;
+    }
+public:
+    ListNode* removeElements(ListNode* Head, int val) {
+        vector<int>v;
+        ListNode* temp = Head;
+        while(temp != NULL){
+            if(temp->val != val){
+                v.push_back(temp->val);
+            }
+            temp = temp->next;
+        }
+        for(auto it : v){
+            insertAtEnd(head, it);
+        }
+        for(auto it : v){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        return head;
+    }
+};
