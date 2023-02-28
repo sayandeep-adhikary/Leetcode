a9309464c94a118b86044dec6ee3a8bc64ec296f
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-02-28 19:06:58 +0530

    Time: 262 ms (51.10%), Space: 118.1 MB (52.75%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    bool isPalindrome(vector<int> arr){
+        int i = 0;
+        int j = arr.size()-1;
+        while(i<=j){
+            if(arr[i] != arr[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    pair<ListNode*, ListNode*> mid(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != NULL and fast->next != NULL){
+            fast = fast -> next -> next;
+            slow = slow->next;
+        }
+        return make_pair(slow, fast);
+    }
+    ListNode* reverse(ListNode* &head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* forward = NULL;
+        while(curr != NULL){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        // vector<int>arr;
+        // ListNode* temp = head;
+        // while(temp!=NULL){
+        //     arr.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+        // return isPalindrome(arr);
+        //*************************************
+        if(head ->next == NULL) return true;
+        ListNode* middle = mid(head).first;
+        ListNode* fast = mid(head).second;
+        ListNode* curr = middle->next;
+        middle->next = reverse(curr);
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+        while(head2 != NULL){
+            if(head1->val != head2->val) return false;
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        return true;
+    }
+};
