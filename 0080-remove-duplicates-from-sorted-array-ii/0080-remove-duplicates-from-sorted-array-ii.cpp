48c15703541e6a41ebc4caf24dbe67b271d59c31
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-02-28 13:13:31 +0530

    Time: 12 ms (18.33%), Space: 11.9 MB (7.97%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -7,8 +7,6 @@ public:
         int i=0;
         int j=nums.size()-1;
         while(i<=j){
-            cout<<"In the loop " <<nums[i]<<"->"<<mp[nums[i]]<<endl;
-            cout<<"In the loop " <<nums[j]<<"->"<<mp[nums[j]]<<endl;
             if(mp[nums[i]] < 2){
                 v.push_back(nums[i]);
             }
@@ -17,26 +15,14 @@ public:
             if(mp[nums[j]] < 2){
                 v.push_back(nums[j]);
             }
-            cout<<"In the loop the vector is : ";
-            for(auto it : v){
-                cout<<it<<" ";
-            }cout<<endl;
             mp[nums[j]]++;
             i++;
             j--;
         }
-        for(auto it : mp){
-            cout<<it.first<<"->"<<it.second<<endl;
-        }
-        cout<<endl;
-        for(auto it : v) cout<<it<<" ";
-        cout<<endl;
         sort(v.begin(), v.end());
-        for(auto it : v) cout<<it<<" ";
         for(int i=0; i<v.size(); i++){
             nums[i] = v[i];
         }
-        cout<<"Ans = "<<v.size();
         return v.size();
     }
 };
