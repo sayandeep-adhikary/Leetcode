2e36f2196562089fe7a7a8170385dd3de96a313a
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-03-11 21:03:36 +0530

    Time: 34 ms (51.33%), Space: 23.3 MB (75.96%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,6 +11,18 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //Iterative Approach
+        // while(root != NULL){
+        //     if(root->val > p->val and root->val > q->val){
+        //         root = root->left;
+        //     }
+        //     if(root->val < p->val and root->val < q->val){
+        //         root = root->right;
+        //     }
+        //     else return root;
+        // }
+        //******************************************************
+        //Recursive Approach
         if(root == NULL) return NULL;
         if(root->val > p->val and root->val > q->val){
             return lowestCommonAncestor(root->left, p, q);
