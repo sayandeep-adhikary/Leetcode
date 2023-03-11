acf216d2e983cbec7b13766887d985fd18455a5c
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-03-11 20:20:14 +0530

    Time: 16 ms (40.98%), Space: 21.8 MB (55.61%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -17,13 +17,24 @@ private:
         v.push_back(root->val);
         inorder(root->right, v);
     }
+    //2nd approach
+    bool isBST(TreeNode* root, long long min, long long max){
+        if(root == NULL) return true;
+        if(root->val > min and root->val < max){
+            bool left = isBST(root->left, min, root->val);
+            bool right = isBST(root->right, root->val, max);
+            return left and right;
+        }
+        else return false;
+    }
 public:
     bool isValidBST(TreeNode* root) {
-        vector<int>v;
-        inorder(root, v);
-        for(int i=1; i<v.size(); i++){
-            if(v[i]<=v[i-1]) return false;
-        }
-        return true;
+        // vector<int>v;
+        // inorder(root, v);
+        // for(int i=1; i<v.size(); i++){
+        //     if(v[i]<=v[i-1]) return false;
+        // }
+        // return true;
+        return isBST(root, LONG_MIN, LONG_MAX);
     }
 };
