6282b5b4fb31a7cedafee8878a70131fded8ec82
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-03-17 22:52:52 +0530

    Time: 710 ms (35.79%), Space: 180.2 MB (19.61%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -23,21 +23,41 @@ private:
         tail = temp;
         temp->next = NULL;
     }
-public:
-    ListNode* swapNodes(ListNode* head, int k) {
-        vector<int>v;
+    int getLength(ListNode* head){
+        int count = 0;
         ListNode* curr = head;
-        ListNode* ansHead = NULL;
-        ListNode* tail = NULL;
         while(curr != NULL){
-            v.push_back(curr->val);
+            count++;
             curr = curr->next;
         }
-        swap(v[k-1], v[v.size() - k]);
-        cout<<endl;
-        for(auto it : v){
-            createList(ansHead, it, tail);
-        }
-        return ansHead;
+        return count;
+    }
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        // vector<int>v;
+        // ListNode* curr = head;
+        // ListNode* ansHead = NULL;
+        // ListNode* tail = NULL;
+        // while(curr != NULL){
+        //     v.push_back(curr->val);
+        //     curr = curr->next;
+        // }
+        // swap(v[k-1], v[v.size() - k]);
+        // cout<<endl;
+        // for(auto it : v){
+        //     createList(ansHead, it, tail);
+        // }
+        // return ansHead;
+        //***********************************
+        int l = getLength(head);
+        ListNode* first = head;
+        ListNode* second = head;
+        for(int i=1; i<k; i++) first = first->next;
+        for(int i=1; i<l-k+1; i++) second = second->next;
+        int temp1 = first->val;
+        int temp2 = second->val;
+        first->val = temp2;
+        second->val = temp1;
+        return head;
     }
 };
