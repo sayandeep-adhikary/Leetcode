998731a3d7037c03bd7c5e6acf8fc9aea3ff8ffc
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-03-17 21:24:19 +0530

    Time: 12 ms (88.40%), Space: 15.7 MB (46.27%) - LeetHub

---
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    int sumDigit(int n){
+        int i=0;
+        while(n != 0){
+            i += n%10;
+            n /= 10;
+        }
+        return i;
+    }
+public:
+    int differenceOfSum(vector<int>& nums) {
+        int ans = 0;
+        int sum1 = accumulate(nums.begin(), nums.end(), 0);
+        int sum2 = 0;
+        for(auto it : nums){
+            sum2 += sumDigit(it);
+        }
+        return abs(sum1-sum2);
+    }
+};
