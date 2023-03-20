a995c607e33030f663e636fad7c5b6b3d52202ce
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-03-20 22:15:11 +0530

    Time: 12 ms (93.73%), Space: 20.3 MB (76.11%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& f, int n) {
+        int s = f.size();
+        if(n == 0) return true;
+        if((n == 1) and (f[0] == 0) and (s == 1)) return true;
+        for(int i=0; i<s; i++){
+            if(f[i] == 0){
+                if(i == 0){
+                if(f[i+1] != 1) {
+                    f[i] = 1;
+                    n--;
+                }
+            }
+            else if(i == s-1){
+                if(f[i-1] != 1){
+                    f[i] = 1;
+                    n--;
+                }
+            }
+            else if(f[i-1] != 1 and f[i+1] != 1){
+                f[i] = 1;
+                n--;
+            }
+            }
+            
+        }
+        return n<=0;
+    }
+};
