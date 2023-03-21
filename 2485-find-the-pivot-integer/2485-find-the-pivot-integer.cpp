6f9b2e9f24e4c07ff469c3a600ccaa657ae0dc7f
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-03-22 00:08:08 +0530

    Time: 0 ms (100.00%), Space: 6 MB (18.44%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        vector<int>v1;
-        vector<int>v2;
-        v1.push_back(1);
-        v2.push_back(n);
-        for(int i=1; i<n; i++){
-            v1.push_back(v1.back()+i+1);
-        }
-        for(int i=1; i<n; i++){
-            v2.push_back(v2.back()+n-i);
-        }
-        reverse(v2.begin(), v2.end());
-        for(int i=0; i<v1.size(); i++){
-            if(v1[i] == v2[i]) return i+1;
-        }
-        return -1;
+        // vector<int>v1;
+        // vector<int>v2;
+        // v1.push_back(1);
+        // v2.push_back(n);
+        // for(int i=1; i<n; i++){
+        //     v1.push_back(v1.back()+i+1);
+        // }
+        // for(int i=1; i<n; i++){
+        //     v2.push_back(v2.back()+n-i);
+        // }
+        // reverse(v2.begin(), v2.end());
+        // for(int i=0; i<v1.size(); i++){
+        //     if(v1[i] == v2[i]) return i+1;
+        // }
+        // return -1;
+        int sum = n * (n + 1) / 2;
+        int x = sqrt(sum);
+        return x * x == sum ? x : -1;
     }
 };
