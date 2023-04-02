149a603635f05745371ba63474fd9cbffcb3e49d
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-04-02 20:50:30 +0530

    Time: 223 ms (96.03%), Space: 97.1 MB (94.04%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n = spells.size();
+        int m = potions.size();
+        vector<int> pairs(n, 0);
+        sort(potions.begin(), potions.end());
+        for (int i = 0; i < n; i++) {
+            int spell = spells[i];
+            int l = 0;
+            int r = m - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                long long product = (long long)spell * (long long)potions[mid];
+                if (product >= success) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            pairs[i] = m - l;
+        }
+        return pairs;
+    }
+};
