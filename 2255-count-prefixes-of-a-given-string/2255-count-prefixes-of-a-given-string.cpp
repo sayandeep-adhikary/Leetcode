3dba963648eb788b962be44727a386205ac4fc28
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-04-05 21:00:58 +0530

    Time: 12 ms (37.89%), Space: 12 MB (22.19%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -3,7 +3,6 @@ public:
     int countPrefixes(vector<string>& words, string s) {
         int count = words.size();
         for(auto it : words){
-            cout<<it<<" ";
             for(int i=0; i<it.size(); i++){
                 if(it[i] != s[i]){
                     count--;
