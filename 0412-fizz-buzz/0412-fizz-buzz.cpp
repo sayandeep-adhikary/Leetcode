04d5591e60e15969342369cbf175af4bb88def33
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-04-09 20:51:15 +0530

    Time: 3 ms (77.18%), Space: 7.9 MB (42.48%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
+    string convert(int n){
+        int temp = n;
+        int rem;
+        string s;
+        while(temp != 0){
+            rem = temp % 10;
+            s.push_back(48+rem);
+            temp /= 10;
+        }
+        reverse(s.begin(), s.end());
+        return s;
+    }
     vector<string> fizzBuzz(int n) {
         vector<string>ans;
         for(int i=1; i<=n; i++){
             if(i%15 == 0) ans.push_back("FizzBuzz");
             else if(i%3 == 0) ans.push_back("Fizz");
             else if(i%5 == 0) ans.push_back("Buzz");
-            else ans.push_back(to_string(i));
+            else ans.push_back(convert(i));
         }
         return ans;
     }
