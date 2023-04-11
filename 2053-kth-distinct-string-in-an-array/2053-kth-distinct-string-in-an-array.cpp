ecc33849c9ce5827af640c1e8522dce2a0f023a0
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-04-11 15:22:53 +0530

    Time: 27 ms (31.76%), Space: 14.6 MB (78.31%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        map<string, int>mp;
+        for(auto it : arr){
+            mp[it]++;
+        }
+        for(auto it : arr){
+            if(mp[it] == 1) k--;
+            if(k == 0) return it;
+        }
+        if(k) return "";
+        return "";
+    }
+};
