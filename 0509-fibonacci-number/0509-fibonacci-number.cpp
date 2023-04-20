97735d2620df0add666cb13fb7072f7d80a40410
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-04-20 20:46:59 +0530

    Time: 0 ms (100.00%), Space: 6 MB (69.41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
+        if(n<=1) return n;
+        int prev2 = 0;
+        int prev = 1;
+        for(int i=1; i<n; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        if(n==1){
-            return 1;
-        }
-        int dp[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        return prev;
     }
 };
