a23106d1f38d428574e3c89bb745f801aa9fa6f7
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-04-25 13:15:03 +0530

    Time: 11 ms (24.80%), Space: 13.6 MB (23.47%) - LeetHub

---
+++ 2399-check-distances-between-same-letters/2399-check-distances-between-same-letters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        vector<int>v(26, -1);
+        map<char, int>mp; 
+        for(int i=0; i<s.size(); i++){
+            int vidx = s[i] - 'a';
+            if(v[vidx] == -1) v[vidx] = i; 
+        }
+        // for(auto it : v){
+        //     cout<<it<<" ";
+        // }
+        // cout<<endl;
+        for(int i=0; i<s.size(); i++){
+            int vidx = s[i] - 'a';
+            mp[s[i]]++;
+            if((v[vidx] != -1) and (mp[s[i]] == 2)){
+                if((i - v[vidx] -1) != distance[vidx]) return false;
+            }
+        }
+        return true;
+    }
+};
