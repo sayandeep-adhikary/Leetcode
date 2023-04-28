90075d62a68bd0f493874a7fe971ec466d3b1cd8
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-04-28 14:11:52 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (46.15%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -5,7 +5,6 @@ public:
         string word;
         vector<int>ans;
         while(ss >> word) if(isdigit(word[0])) ans.push_back(stoi(word));
-        // for(auto it : ans) cout<<it<<" ";
         for(int i=1; i<ans.size(); i++){
             if(ans[i] <= ans[i-1]) return false;
         }
