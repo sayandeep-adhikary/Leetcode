9f16b8ff0bd5be3ac037ae41cbb779e4df9d5692
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-05-08 21:59:49 +0530

    Time: 15 ms (18.94%), Space: 9.4 MB (69.95%) - LeetHub

---
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double trimMean(vector<int>& arr) {
+        int n = arr.size();
+        // cout<<n<<endl;
+        int x = n / 20;
+        double sum = 0;
+        double ans;
+        sort(arr.begin(), arr.end());
+        for(int i=x; i<arr.size()-x; i++) sum += arr[i];
+        return sum / (n - (2*x));
+    }
+};
