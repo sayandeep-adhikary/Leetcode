482bbbff474e5b1671bc306ba6f31f17455bff05
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-08-20 12:16:56 +0530

    Time: 101 ms (77.87%), Space: 61.3 MB (43.00%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // map<int, int>mp;
-        // for(auto it : nums){
-        //     mp[it]++;
-        // }
-        // for(auto it : mp){
-        //     if(it.second > 1) return it.first;
-        // }
-        // return 0; 
         sort(nums.begin(), nums.end());
         for(int i=1; i<nums.size(); i++){
-            if(nums[i] == nums[i-1]){
-                return nums[i];
-            }
+            if(nums[i] == nums[i-1]) return nums[i];
         }
         return 0;
     }
