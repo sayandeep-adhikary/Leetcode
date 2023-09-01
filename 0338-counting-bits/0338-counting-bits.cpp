ea30bd03cb9758d8afa3733b96c08f4c73b6b72e
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-09-01 10:53:26 +0530

    Time: 3 ms (89.82%), Space: 8 MB (45.56%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,10 @@
 class Solution {
-private:
-    int bitCount(int n){
-        int ans = 0;
-        while(n){
-            if(n&1) ans++;
-            n = n >> 1;
-        }
-        return ans;
-    }
 public:
     vector<int> countBits(int n) {
-        vector<int>ans;
-        for(int i=0; i<=n; i++){
-            ans.push_back(bitCount(i));
+        vector<int>ans(n+1);
+        ans[0] = 0;
+        for(int i=1; i<=n; i++){
+            ans[i] = ans[i/2] + i%2;
         }
         return ans;
     }
