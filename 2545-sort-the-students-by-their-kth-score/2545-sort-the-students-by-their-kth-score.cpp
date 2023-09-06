0d61450618618a13553547c958b24199d4fb4f58
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-09-07 00:07:42 +0530

    Time: 43 ms (97.27%), Space: 27.3 MB (88.64%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
         sort(score.begin(), score.end(),
-          [k] (const std::vector<int> &a, const std::vector<int> &b)
+          [k] (vector<int> &a, vector<int> &b)
           {
               return a[k] > b[k];
           });
