f9ef4e42451ce2bfbafa0d45f2224ad0c7c8ef53
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-09-06 23:44:31 +0530

    Time: 20 ms (15.04%), Space: 13.4 MB (37.79%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& g) {
+        vector<vector<int>>ans;
+        unordered_map<int, vector<int>>mp;
+        int i=0;
+        for(auto it : g) mp[it].push_back(i++);
+        for(auto it : mp){
+            cout<<it.first<<"-> [";
+            for(auto a : it.second) cout<<a<<" ";
+            cout<<"]"<<endl;
+        }
+        for(auto it : mp){
+            int temp = it.first;
+            int i = 0;
+            vector<int>v;
+            while(i < it.second.size()){
+                if(i == 0 or i % temp != 0) v.push_back(it.second[i]);
+                else{
+                    ans.push_back(v);
+                    v.clear();
+                    v.push_back(it.second[i]);
+                }
+                i++;
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
