0cd13e6c522aac6c120ccd2570fd9f404c8f837e
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-09-08 13:56:34 +0530

    Time: 23 ms (79.69%), Space: 8.8 MB (76.35%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    int getPower(int i){
+        int cnt = 0;
+        // int j = i;
+        while(i != 1){
+            // if(dp[i] != -1){
+            //     return cnt + dp[i];
+            // }
+            cnt++;
+            if(i&1){
+                i = (3*i) + 1;
+            }
+            else{
+                i = i / 2;
+            }
+        }
+        // dp[j] = i;
+        return cnt;
+    }
+public:
+    static bool cmp(pair<int, int>&a, pair<int, int>&b){
+        if(a.second == b.second) return a < b;
+        else return a.second < b.second;
+    }
+    int getKth(int lo, int hi, int k) {
+        // vector<int>dp(1001, -1);
+        vector<pair<int, int>>v;
+        for(int i=lo; i<=hi; i++){
+            int power = getPower(i);
+            v.push_back({i, power});
+        }
+        sort(v.begin(), v.end(), cmp);
+        // cout<<"Dp is : ";
+        // for(auto it : dp) cout<<it<<" ";
+        // cout<<endl;
+        // for(auto it : v){
+        //     cout<<it.first<<"->"<<it.second<<endl;
+        // }
+        return v[k-1].first;
+    }
+};
