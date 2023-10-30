9ec3b85bd38ea504e9e89499ce32e1f40227eacd
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-10-30 10:12:48 +0530

    Time: 9 ms (43.13%), Space: 10.4 MB (81.00%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,27 +1,11 @@
 class Solution {
-private:
-    int getBits(int n){
-        int count = 0;
-        while (n){
-            if(n & 1)count++;
-            n >>= 1;
-        }
-    return count;
-    }
 public:
-    static bool cmp(pair<int, int>&a, pair<int, int>&b){
-        if(a.second == b.second) return a.first < b.first;
-        else return a.second < b.second;
-    }
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int, int>>v;
-        vector<int>ans;
-        for(auto it : arr){
-            int cnt = getBits(it);
-            v.push_back({it, cnt});
-        }
-        sort(v.begin(), v.end(), cmp);
-        for(auto it : v) ans.push_back(it.first);
-        return ans;
+        sort(arr.begin(), arr.end(), [](const int&a, const int&b){
+           int countA = __builtin_popcount(a);
+            int countB = __builtin_popcount(b);
+            return countA == countB ? a < b : countA < countB;
+        });
+        return arr;
     }
 };
