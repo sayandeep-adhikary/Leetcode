5618971f0bdef0c0f4fbbaaced9338247cbb3982
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-11-05 11:23:00 +0530

    Time: 16 ms (14.63%), Space: 6.5 MB (19.06%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int n = s.size();
+        int l = 0; int r = n-1;
+        while(l<r){
+            if(s[l]!=s[r]) return false;
+            l++;r--;
+        }
+        return true;
+            
+        }
+        
+    
+};
