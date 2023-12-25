6946f275b132ca1edbd36cf43b69bd168111cfb7
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2023-12-25 20:19:44 +0530

    Time: 3 ms (83.89%), Space: 11 MB (22.63%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int>mp;
+        vector<int>ans;
+        for(auto it : nums1) mp[it]++;
+        for(auto it : nums2){
+            if(mp[it] > 0){
+                ans.push_back(it);
+                mp[it]--;
+            }
+        }
+        return ans;
+    }
+};
