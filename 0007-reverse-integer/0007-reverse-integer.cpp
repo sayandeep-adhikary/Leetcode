573fecfb09fb56f8142bd3f780e9d99a6bc83e6f
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-15 20:29:12 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (5.42%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x){
+            int digit = x % 10;
+            if((ans > INT_MAX / 10) or (ans < INT_MIN / 10)) return 0;
+            ans = (ans * 10) + digit;
+            x /= 10;
+        }
+        return ans;
+    }
+};
