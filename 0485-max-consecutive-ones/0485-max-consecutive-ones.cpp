b09d4c36e338807f60d5186cc8e2de306eede208
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-19 23:41:05 +0530

    Time: 30 ms (27.41%), Space: 38.8 MB (6.18%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans = 0;
+        int count = 0;
+        for(auto it : nums){
+            if(it == 1){
+                count++;
+            }
+            else{
+                ans = max(ans, count);
+                count = 0;
+            }
+        }
+        ans = max(ans, count);
+        return ans;
+    }
+};
