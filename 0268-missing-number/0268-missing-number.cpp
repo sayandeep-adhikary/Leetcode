7c878fe973c5a9da49700e9f176bd417e02b16f8
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-19 23:34:23 +0530

    Time: 3 ms (99.86%), Space: 20.7 MB (27.48%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1 = 0, xor2 = 0;
+        for(int i=0; i<nums.size(); i++){
+            xor1 = xor1 ^ (i+1);
+            xor2 = xor2 ^ nums[i];
+        }
+        return xor1 xor xor2;
+    }
+};
