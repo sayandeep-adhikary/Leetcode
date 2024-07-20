dbe204bb319234d0dd6af1f93b178f9859a4d1be
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-20 20:25:42 +0530

    Time: 4 ms (94.07%), Space: 14.4 MB (15.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,48 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //Brute Force approach, TC = O(n^2), SC = O(1)
-//         for(int i=0; i<nums.size();i++)
-//         {
-//             for(int j = i+1; j<nums.size();j++)
-//             {
-//                 if(nums[i]+nums[j]==target )
-//                 {
-//                     return {i, j};
-//                     break;                
-//                 }           
-//             }
-//         }
-//         return {-1, -1};
-        
-        
-        //Using unordered map : TC = O(n), SC  = O(n)
-        unordered_map<int, int>m;
+        unordered_map<int, int>mp;
         for(int i=0; i<nums.size(); i++){
-            int x = nums[i];
-            int y = target - x;
-            if(m.find(y) != m.end()){
-                return {i, m[y]};
+            int num = nums[i];
+            int more = target - num;
+            if(mp.find(more) != mp.end()){
+                return {mp[more], i};
             }
-            m[nums[i]] = i;
+            mp[num] = i;
         }
-        return{-1, -1};
-        
-        // two pointer approach : TC = O(nlogn), SC = O(n)
-        // int n = nums.size();
-        // vector<pair<int, int>>v;
-        // for(int i=0; i<n; i++){
-        //     v.emplace_back(nums[i], i);
-        // }
-        // sort(v.begin(), v.end());
-        // int start = 0;
-        // int end = nums.size() - 1;
-        // while(start<end){
-        //     int sum = v[start].first + v[end].first;
-        //     if(sum==target) return{v[start].second, v[end].second};
-        //     else if(sum>target) end--;
-        //     else start++;
-        // }
-        // return {-1, -1};
+        return {};
     }
 };
