630c69c31521d05f8f4b41c8fa0866a0c367fa95
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-20 23:16:32 +0530

    Time: 77 ms (58.43%), Space: 70.6 MB (14.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum = 0;
-        int maxi = INT_MIN;
-        //Kadane's Algorithm
-        for(auto it : nums){
-            sum += it;
+        int sum = 0, maxi = INT_MIN;
+        for(int i=0; i<nums.size(); i++){
+            sum += nums[i];
             maxi = max(maxi, sum);
-            if(sum<0) sum = 0;
+            if(sum < 0) sum = 0;
         }
         return maxi;
-        //*****************************************
-        //Brute Force Approach
-        // for(int i=0; i<nums.size(); i++){
-        //     int curr_sum = 0;
-        //     for(int j=i; j<nums.size(); j++){
-        //         curr_sum += nums[j];
-        //         maxi = max(maxi, curr_sum);
-        //     }
-        // }
-        // return maxi;
     }
 };
