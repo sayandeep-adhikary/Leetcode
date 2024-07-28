d21f0af63f5f87f66e241fbbe3280ac71f2ffe28
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-28 23:45:34 +0530

    Time: 147 ms (42.97%), Space: 126.5 MB (55.64%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
-        vector<int>ans;
-        for(auto it : nums){
-            if(it < 0) neg.push_back(it);
-            else pos.push_back(it);
-        }
-        int a=0, b=0;
+        // Brute Force
+        // vector<int>pos;
+        // vector<int>neg;
+        // vector<int>ans;
+        // for(auto it : nums){
+        //     if(it < 0) neg.push_back(it);
+        //     else pos.push_back(it);
+        // }
+        // int a=0, b=0;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(i&1) ans.push_back(neg[a++]);
+        //     else ans.push_back(pos[b++]);
+        // }
+        // return ans;
+        int pos = 0;
+        int neg=1;
+        vector<int>ans(nums.size());
         for(int i=0; i<nums.size(); i++){
-            if(i&1) ans.push_back(neg[a++]);
-            else ans.push_back(pos[b++]);
+            if(nums[i]>0){
+                ans[pos] = nums[i];
+                pos+=2;
+            }
+            else{
+                ans[neg] = nums[i];
+                neg += 2;
+            }
         }
         return ans;
     }
