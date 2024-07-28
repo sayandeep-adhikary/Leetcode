c442cbfaa4565a0bc984c51d10234cba26ec2e67
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-29 00:19:29 +0530

    Time: 4 ms (55.75%), Space: 14.9 MB (14.27%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int index = -1;
-        int n = nums.size();
-        for(int i=n-2; i>=0; i--){
+        for(int i=nums.size()-2; i>=0; i--){
             if(nums[i] < nums[i+1]){
                 index = i;
                 break;
@@ -11,17 +10,15 @@ public:
         }
         if(index == -1){
             reverse(nums.begin(), nums.end());
-            return;
-        }
-        else{
-            int num = nums[index];
-            for(int i=n-1; i>index; i--){
-                if(nums[i] > nums[index]){
-                    swap(nums[i], nums[index]);
-                    break;
-                }
+        }else{
+            for(int i=nums.size()-1; i>index; i--){
+            if(nums[i] > nums[index]){
+                swap(nums[i], nums[index]);
+                break;
             }
         }
         reverse(nums.begin()+index+1, nums.end());
+        }
+        
     }
 };
