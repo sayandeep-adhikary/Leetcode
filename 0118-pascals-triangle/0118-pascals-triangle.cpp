17a6f3f22e3b1f2b4bec877991ffb1f11b18d248
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-29 21:15:57 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (15.26%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
+    vector<int> generateRow(int row){
+        long long ans = 1;
+        vector<int>ansRow;
+        ansRow.push_back(ans);
+        for(int col=1; col<row; col++){
+            ans = ans * (row - col);
+            ans /= col;
+            ansRow.push_back(ans);
+        }
+        return ansRow;
+    }
     vector<vector<int>> generate(int numRows) {
-	vector<vector<int>> ans;
-	for (int i = 0; i < numRows; i++) {
-		vector<int> row(i + 1, 1);
-		for (int j = 1; j < i; j++) {
-			row[j] = ans[i - 1][j] + ans[i - 1][j - 1];
-		}
-		ans.push_back(row);
-	}
-	return ans;
-}
+        vector<vector<int>>ans;
+        for(int i=1; i<=numRows; i++){
+            ans.push_back(generateRow(i));
+        }
+        return ans;
+    }
 };
