349eb1e429ea0732fcd67161c82a362dfeb02f25
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-30 00:25:54 +0530

    Time: 28 ms (55.65%), Space: 16.2 MB (44.37%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
+        for(int i=0; i<nums.size(); i++){
+            if(i>0 and nums[i] == nums[i-1]) continue;
+            for(int j=i+1; j<nums.size(); j++){
+                if(j>i+1 and nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = nums.size() - 1;
+                while(k < l){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        k++;
+                        l--;
+                        while(k<l and nums[k] == nums[k-1]) k++;
+                        while(k<l and nums[l] == nums[l+1]) l--;
+                    }
+                    else if(sum < target) k++;
+                    else l--;
+                }
+            }
+        }
+        return ans;
+    }
+};
