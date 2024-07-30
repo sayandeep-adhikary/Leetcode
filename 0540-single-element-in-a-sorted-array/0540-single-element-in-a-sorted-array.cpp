5453c4416b0085483d814ee96f620562ff0e0637
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-30 21:45:41 +0530

    Time: 22 ms (31.60%), Space: 24.8 MB (36.06%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[nums.size()-1] != nums[nums.size()-2]) return nums[nums.size()-1];
+        int low = 1, high = nums.size()-2;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] != nums[mid-1] and nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            else if((mid&1 and nums[mid] == nums[mid-1]) or 
+                    (!(mid&1) and nums[mid] == nums[mid+1])){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+};
