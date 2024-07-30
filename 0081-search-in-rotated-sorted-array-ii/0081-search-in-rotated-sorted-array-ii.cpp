78e025ff5a4fa9025015f0f4f600a36e05693745
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-30 15:43:26 +0530

    Time: 10 ms (12.24%), Space: 16.8 MB (10.27%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        return find(nums.begin(), nums.end(), target) != nums.end();
+        int low = 0, high = nums.size()-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid] == target) return true;
+            if(nums[low] == nums[mid] and nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;                
+            }
+            if(nums[low]<=nums[mid]){
+                if(nums[low] <= target and target <= nums[mid]){
+                    high = mid-1;
+                }
+                else low = mid+1;
+            }
+            else {
+                if(nums[mid] <= target and target <= nums[high]){
+                    low = mid+1;
+                }
+                else high = mid-1;
+            }
+        }
+        return false;
     }
 };
