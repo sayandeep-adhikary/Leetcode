932f34dc0f147134d5c60ac5ffbe901319077d50
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-30 13:44:10 +0530

    Time: 3 ms (71.31%), Space: 12.4 MB (18.84%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int i=0, j=nums.size()-1;
-        int ans = nums.size();
-        while(i<=j){
-            int mid = i + (j-i)/2;
-            if(nums[mid] >= target){
-                ans = mid;
-                j = mid-1;
-            }
-            else{
-                i = mid+1;
-            }
-        }
-        return ans;
+        // int i=0, j=nums.size()-1;
+        // int ans = nums.size();
+        // while(i<=j){
+        //     int mid = i + (j-i)/2;
+        //     if(nums[mid] >= target){
+        //         ans = mid;
+        //         j = mid-1;
+        //     }
+        //     else{
+        //         i = mid+1;
+        //     }
+        // }
+        // return ans;
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
     }
 };
