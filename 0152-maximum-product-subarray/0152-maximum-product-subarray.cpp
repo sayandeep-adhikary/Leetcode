d97b5a698c64b559fe9b595fda68ba27e690090d
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-30 11:37:40 +0530

    Time: 7 ms (35.03%), Space: 16.4 MB (35.91%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        double maxi = INT_MIN;
+        double pre = 1, suff = 1;
+        for(int i=0; i<nums.size(); i++){
+            if(pre == 0) pre = 1;
+            if(suff == 0) suff = 1;
+            pre = pre * nums[i];
+            suff = suff * nums[nums.size()-i-1];
+            maxi = max(maxi, max(pre, suff));
+        }
+        return maxi;
+    }
+};
