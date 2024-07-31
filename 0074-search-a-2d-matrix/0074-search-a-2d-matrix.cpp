d1f34450f730c662aa576829c0a85af850bbf4c3
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-31 23:42:02 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (13.57%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -13,11 +13,21 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        for(auto it : matrix){
-            if(it[0] <= target and it[m-1] >= target){
-                cout << m << endl;
-                return isPresent(it, m, target);
-            }
+        // for(auto it : matrix){
+        //     if(it[0] <= target and it[m-1] >= target){
+        //         cout << m << endl;
+        //         return isPresent(it, m, target);
+        //     }
+        // }
+        // return false;
+        int low = 0, high = (n*m) - 1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            int r = mid/m;
+            int c = mid%m;
+            if(matrix[r][c] == target) return true;
+            else if(matrix[r][c] > target) high = mid-1;
+            else low = mid+1;
         }
         return false;
     }
