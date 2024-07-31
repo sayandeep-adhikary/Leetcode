1494b3c03eea3e38f442a40946a67c04e5be354d
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-07-31 11:48:48 +0530

    Time: 34 ms (47.71%), Space: 37.9 MB (6.47%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int H) {
-        int low = 1, high = 10000000000, k = 0;
-        while (low <= high) {
-            k = (low + high) / 2;
-            int h = 0;
-            for (int i = 0; i < piles.size(); i ++) 
-                h += ceil(1.0 * piles[i] / k);
-            if (h > H)
-                low = k + 1;
-            else
-                high = k - 1;
+    long long func(vector<int> piles, int h){
+        long long ans = 0;
+        for(auto it : piles) ans += ceil((double)it/(double)h);
+        return ans;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            long long timeToEat = func(piles, mid);
+            if(timeToEat <= h){
+                high = mid-1;
+            }
+            else low = mid+1;
         }
         return low;
     }
