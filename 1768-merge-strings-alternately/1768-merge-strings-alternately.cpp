55c965933c757effb89242655f44c0123d55d145
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-01 21:01:24 +0530

    Time: 3 ms (45.13%), Space: 7.9 MB (19.45%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i=0;
-        int j=0;
         string ans = "";
+        int i=0, j=0;
         while(i<word1.size() and j<word2.size()){
-            ans.push_back(word1[i++]);
-            ans.push_back(word2[j++]);
+            ans += word1[i++];
+            ans += word2[j++];
         }
-        while(i<word1.size()) ans.push_back(word1[i++]);
-        while(j<word2.size()) ans.push_back(word2[j++]);
+        while(i<word1.size()) ans += word1[i++];
+        while(j<word2.size()) ans += word2[j++];
         return ans;
     }
 };
