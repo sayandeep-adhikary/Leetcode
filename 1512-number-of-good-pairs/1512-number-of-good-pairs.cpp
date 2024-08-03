32dcf52ec42a5dcfdb56ea33c514a2a16d017230
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-03 12:55:21 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (7.73%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int ans = 0;
-        for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i] == nums[j]) ans++;
-            }
+        unordered_map<int, int>mp;
+        for(auto it : nums) mp[it]++;
+        int count = 0;
+        for(auto it : mp){
+            int f = it.second;
+            count += (f*(f-1))/2;
         }
-        return ans;
+        return count;
     }
 };
