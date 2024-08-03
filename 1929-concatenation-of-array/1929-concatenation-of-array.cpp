fa08206cab8e1fffe63b1ca5efafaafb4e4f403e
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-03 12:46:18 +0530

    Time: 8 ms (32.84%), Space: 15.2 MB (57.32%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int>ans = nums;
+        vector<int>ans(2*nums.size());
         for(int i=0; i<nums.size(); i++){
-            ans.push_back(nums[i]);
+            ans[i] = nums[i];
+            ans[i+nums.size()] = nums[i];
         }
         return ans;
     }
