06d55cdf8d39b1959837a6913cd0f1169caaab6b
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-03 13:26:56 +0530

    Time: 7 ms (50.50%), Space: 24.4 MB (13.21%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int count = 0;
+        for(auto it : nums){
+            if(it < k) count++;
+        }
+        return count;
+    }
+};
