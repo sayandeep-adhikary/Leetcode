f283954b77c109e2a5e21a955c2944282920dc9c
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-10 23:25:46 +0530

    Time: 11 ms (11.90%), Space: 16.3 MB (40.76%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
-    int firstOcc(vector<int> nums, int target){
-        int low = 0, high = nums.size()-1;
+    int getFirstOcc(vector<int>& nums, int target){
         int ans = -1;
+        int low = 0, high = nums.size() - 1;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid] == target){
                 ans = mid;
                 high = mid-1;
             }
-            else if(nums[mid] > target){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
+            else if(nums[mid] > target) high = mid-1;
+            else low = mid+1;
         }
         return ans;
     }
-    int lastOcc(vector<int> nums, int target){
-        int low = 0, high = nums.size()-1;
+    int getLastOcc(vector<int>& nums, int target){
         int ans = -1;
+        int low = 0, high = nums.size() - 1;
         while(low<=high){
             int mid = low + (high-low)/2;
             if(nums[mid] == target){
                 ans = mid;
                 low = mid+1;
             }
-            else if(nums[mid] > target){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
+            else if(nums[mid] > target) high = mid-1;
+            else low = mid+1;
         }
         return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int f = firstOcc(nums, target);
-        int l = lastOcc(nums, target);
-        return {f, l};
+        int firstOcc = getFirstOcc(nums, target);
+        int lastOcc = getLastOcc(nums, target);
+        return {firstOcc, lastOcc};
     }
 };
