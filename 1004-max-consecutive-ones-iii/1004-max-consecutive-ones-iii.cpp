ddecbb20dcff6e492e417a53e12555f3e8c5104b
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-15 15:24:11 +0530

    Time: 34 ms (87.17%), Space: 58.2 MB (8.36%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,14 +4,24 @@ public:
         int left = 0, right = 0, maxLen = 0;
         int zeroCount = 0;
         int n = nums.size();
+        // while(right<n){
+        //     if(nums[right] == 0) zeroCount++;
+        //     while(zeroCount > k){
+        //         if(nums[left] == 0) zeroCount--;
+        //         left++;
+        //     }
+        //     int len = right - left + 1;
+        //     maxLen = max(len, maxLen);
+        //     right++;
+        // }
+        // return maxLen;
         while(right<n){
             if(nums[right] == 0) zeroCount++;
-            while(zeroCount > k){
+            if(zeroCount > k){
                 if(nums[left] == 0) zeroCount--;
                 left++;
             }
-            int len = right - left + 1;
-            maxLen = max(len, maxLen);
+            if(zeroCount <= k) maxLen = max(maxLen, (right-left+1));
             right++;
         }
         return maxLen;
