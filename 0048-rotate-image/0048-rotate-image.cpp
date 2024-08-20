4d8628376f0b7467b160dcf5fb6aaeeb4f83657b
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-20 11:22:28 +0530

    Time: 0 ms (100.00%), Space: 9 MB (39.71%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -8,8 +8,8 @@ public:
         //     }
         // }
         // matrix = ans;
-        for(int i=0; i<matrix.size()-1; i++){
-            for(int j=i+1; j<matrix.size(); j++){
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=i; j<matrix[0].size(); j++){
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
