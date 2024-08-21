644600509b67d5521dad30b83af8709672257a6e
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-21 23:25:54 +0530

    Time: 3 ms (69.71%), Space: 19.7 MB (5.97%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,90 +1,18 @@
-class TrieNode{
-    public:
-    char data;
-    TrieNode* children[26];
-    int childCount;
-    bool isTerminal;
-    TrieNode(char ch){
-        data = ch;
-        for(int i=0; i<26; i++){
-            children[i] = NULL;
-        }
-        childCount = 0;
-        isTerminal = false;
-    }
-};
-class Trie{
-    public:
-    TrieNode* root;
-    Trie(char ch){
-        root = new TrieNode(ch);
-    }
-    void insertUtil(TrieNode* root, string word){
-        //base case
-        if(word.length() == 0){
-            root->isTerminal = true;
-            return;
-        }
-        //assumption is that all the chars are in caps
-        int index = word[0] - 'a';
-        TrieNode* child;
-        if(root->children[index] != NULL){
-            //present in trie
-            child = root->children[index];
-        }
-        else{
-            //absent in trie
-            child = new TrieNode(word[0]);
-            root->childCount++;
-            root->children[index] = child;
-        }
-        //recursion
-        insertUtil(child, word.substr(1));
-    }
-    void insertword(string word){
-        insertUtil(root, word);
-    }
-    
-    void lcp(string first, string &ans){
-        for(int i=0; i<first.length(); i++){
-            char ch = first[i];
-            if(root->childCount == 1){
-                ans += ch;
-                int index = ch - 'a';
-                root = root->children[index];
-            }
-            else break;
-            if(root->isTerminal) break;
-        }
-    }
-};
 class Solution {
 public:
+    bool isValid(char ch, vector<string> strs, int ind){
+        for(int i=1; i<strs.size(); i++){
+            if(strs[i][ind] != ch) return false;
+        }
+        return true;
+    }
     string longestCommonPrefix(vector<string>& strs) {
-        // string s = "";
-        // bool flag = true;
-        // int size = strs[0].size();
-        // for(int i=0; i<size; i++){
-        //     int ch = strs[0][i];
-        //     for(int j=0; j<strs.size(); j++){
-        //         if(ch != strs[j][i]){
-        //             flag = false;
-        //             break;
-        //         }
-        //     }
-        //     if(flag) s += ch;
-        // }
-        // return s;
-        //*******************************************
-        //Trie approach
-        Trie* t = new Trie('\0');
-        for(auto it : strs){
-            if(it == "")return "";
-            t->insertword(it);
-        } 
-        string first = strs[0];
         string ans = "";
-        t->lcp(first, ans);
+        for(int i=0; i<strs[0].size(); i++){
+            char ch = strs[0][i];
+            if(isValid(ch, strs, i)) ans += ch;
+            else break;
+        }
         return ans;
     }
 };
