3c0ac56d6277025fd047514e3d83a375ed1778e3
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-22 11:30:01 +0530

    Time: 9 ms (51.28%), Space: 8.8 MB (70.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,11 +4,6 @@ public:
         if(s.size() != t.size()) return false;
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        int i=0, j=0;
-        while(i<s.size() and j<t.size()){
-            if(s[i] != t[j]) return false;
-            i++, j++;
-        }
-        return true;
+        return s == t;
     }
 };
