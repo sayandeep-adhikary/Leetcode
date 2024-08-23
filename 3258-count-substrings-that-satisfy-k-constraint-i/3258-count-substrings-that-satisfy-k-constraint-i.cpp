3749b5315b8193e1b4b8c6eaca33591f0bab0225
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-23 16:26:22 +0530

    Time: 3 ms (75.83%), Space: 8.8 MB (68.52%) - LeetHub

--- 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cpp
+++ 3258-count-substrings-that-satisfy-k-constraint-i/3258-count-substrings-that-satisfy-k-constraint-i.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int countKConstraintSubstrings(string s, int k) {
         int count = 0;
-        for(int i=0; i<s.size(); i++){
-            int zero = 0;
-            int one = 0;
-            for(int j=i; j<s.size(); j++){
-                if(s[j] == '1') one++;
-                else zero++;
-                if(zero<=k or one<=k) count++;
-            }  
+        int l=0, r=0;
+        int one = 0, zero = 0;
+        while(r<s.size()){
+            if(s[r] == '1') one++;
+            else zero++;
+            while(one > k and zero > k){
+                if(s[l] == '1') one--;
+                else zero--;
+                l++;
+            }
+            count += (r-l+1);
+            r++;
         }
         return count;
     }
