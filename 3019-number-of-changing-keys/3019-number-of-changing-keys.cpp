670ca246095b33d099cb72fa9dcd60186f72be49
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-23 15:36:33 +0530

    Time: 5 ms (24.94%), Space: 8.7 MB (56.26%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -3,8 +3,14 @@ public:
     int countKeyChanges(string s) {
         int count = 0;
         for(int i=1; i<s.size(); i++){
-            char ch1 = tolower(s[i]);
-            char ch2 = tolower(s[i-1]);
+            char ch1 = s[i];
+            char ch2 = s[i-1];
+            if(ch1>=97 and ch1 <=122){
+                ch1 = ch1 - 32;
+            }
+            if(ch2>=97 and ch2 <=122){
+                ch2 = ch2 - 32;
+            }
             count += ch1 != ch2;
         }
         return count;
