4271292d0d7428a1a12eebfb163ca58da59fa19c
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-27 13:14:43 +0530

    Time: 4 ms (39.33%), Space: 8.9 MB (46.66%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,13 +3,16 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
+        vector<vector<int>> ans(n, vector<int>(m));
         for(int i=0; i<n; i++){
-            for(int j=i; j<m; j++){
-                swap(matrix[i][j], matrix[j][i]);
+            for(int j=0; j<m; j++){
+                ans[j][n-i-1] = matrix[i][j];
             }
         }
-        for(auto &it : matrix){
-            reverse(it.begin(), it.end());
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = ans[i][j];
+            }
         }
     }
 };
