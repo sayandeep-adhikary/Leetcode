2d8fcebf70377f52fc041f32dbfcf69d8280cd0e
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-08-31 13:03:15 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (25.34%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    string getString(string &s){
-        string ans = "";
-        for(int i=1; i<s.size()-1; i++){
-            ans += s[i];
-        }
-        return ans;
-    }
     string removeOuterParentheses(string s) {
+        stack<char>st;
         string ans = "";
-        string temp = "";
-        int cnt1 = 0, cnt2 = 0;
-        for(auto it: s){
-            temp += it;
-            if(it == '(') cnt1++;
-            else cnt2++;
-            if(cnt1 == cnt2){
-                ans += getString(temp);
-                temp = "";
+        for(auto it : s){
+            if(it == '('){
+                if(st.size() != 0){
+                    ans += it;
+                }
+                st.push(it);
+            }
+            else{
+                if(st.size() > 1){
+                    st.pop();
+                    ans += it;
+                }
+                else if(st.size() == 1){
+                    st.pop();
+                }
             }
         }
         return ans;
