79a4622f8c87c9d9c153b5d69f6bfe77420168a6
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-09-01 16:59:19 +0530

    Time: 10 ms (65.65%), Space: 11.4 MB (17.55%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    static bool cmp(pair<char, int>&a, pair<char, int>&b){
-        return a.second > b.second;
-    }
     string frequencySort(string s) {
-        map<char, int>mp;
+        unordered_map<char, int>mp;
+        vector<pair<int, char>>v;
         string ans = "";
         for(auto it : s) mp[it]++;
-        vector<pair<char, int>>v;
-        for(auto it : mp) v.push_back(it);
-        sort(v.begin(), v.end(), cmp);
+        for(auto it: mp) v.push_back({it.second, it.first});
+        sort(v.begin(), v.end(), [](pair<int, char>a, pair<int, char>b){
+            return a.first>b.first;
+        });
         for(auto it : v){
-            for(int i=0; i<it.second; i++) ans += it.first;
+            string temp(it.first, it.second);
+            ans += temp;
         }
         return ans;
     }
