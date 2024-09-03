b80c0983d408e628df7aaa5eec35a7e70c4e003a
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-09-03 21:44:45 +0530

    Time: 107 ms (15.46%), Space: 93.1 MB (6.11%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -2,10 +2,21 @@ class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
         k %= accumulate(chalk.begin(), chalk.end(), 0l);
-        for(int i=0; i<chalk.size(); i++){
-            if((k-chalk[i])<0) return i;
-            else k -= chalk[i];
+        vector<long long>v;
+        int ans;
+        for(auto it : chalk){
+            if(v.size() == 0) v.push_back(it);
+            else v.push_back(v.back()+it);
         }
-        return 0;
+        int l = 0, h = chalk.size() - 1;
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(v[mid] > k){
+                ans = mid;
+                h = mid-1;
+            }
+            else l = mid+1;
+        }
+        return ans;
     }
 };
