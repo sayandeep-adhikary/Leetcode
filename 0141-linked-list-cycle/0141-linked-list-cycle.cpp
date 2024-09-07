61ec5c72955dabbe7ee463fc686d44254f324ede
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-09-07 11:25:34 +0530

    Time: 12 ms (34.04%), Space: 10.8 MB (65.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,37 +9,12 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // map<ListNode*, bool>m;
-        // ListNode* temp = head;
-        // while(temp!=NULL){
-        //     if(m[temp] == true){
-        //         return true;
-        //     }
-        //     m[temp] = true;
-        //     temp = temp->next;
-        // }
-        // return false;
-        // if(head == NULL) return false;
-        //ListNode* slow = head;
-        // ListNode* fast = head;
-        // while(slow!=NULL and fast != NULL){
-        //     fast = fast->next;
-        //     if(fast!= NULL){
-        //         fast = fast->next;
-        //     }
-        //     slow = slow->next;
-        //     if(slow == fast)return true;
-        // }
-        // return false;
-        if(head == NULL) return false;
         ListNode* slow = head;
-        ListNode *fast = head;
-        while(fast != NULL && fast ->next != NULL)
-        {
-            fast = fast->next->next;
+        ListNode* fast = head;
+        while(fast != NULL and fast->next != NULL){
             slow = slow->next;
-            if(fast == slow)
-                return true;
+            fast = fast->next->next;
+            if(slow == fast) return true;
         }
         return false;
     }
