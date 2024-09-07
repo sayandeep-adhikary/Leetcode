646112b4ef30e1af74e1f783be78d0d525eaf195
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-09-07 11:36:05 +0530

    Time: 6 ms (71.35%), Space: 10.2 MB (86.79%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -7,36 +7,22 @@
  * };
  */
 class Solution {
-private:
-    ListNode* detectloop(ListNode *&head){
+public:
+    ListNode *detectCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        while(slow!=NULL and fast!=NULL)
-        {
-            fast = fast->next;
-            if(fast!=NULL)fast = fast->next;
+        while(fast != NULL and fast->next != NULL){
             slow = slow->next;
-            if(fast == slow){
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
                 return slow;
             }
         }
         return NULL;
-    }
-    ListNode* loopNode(ListNode *head){
-        ListNode* fast = detectloop(head);
-        if(fast == NULL) return NULL;
-        ListNode* slow = head;
-        while(slow != fast){
-            slow = slow->next;
-            fast = fast->next;
-        }
-        return slow;
-    }
-public:
-    ListNode *detectCycle(ListNode *head) {
-        if(head == NULL){
-            return NULL;
-        }
-        return loopNode(head);
     }
 };
