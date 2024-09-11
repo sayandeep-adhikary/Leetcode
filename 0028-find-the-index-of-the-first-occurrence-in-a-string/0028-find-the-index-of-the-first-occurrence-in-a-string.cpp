d04b74a0beca71106d8563b8d5bcde38cf18f317
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-09-11 13:31:24 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (65.14%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int l = 0;
-        int r = 0;
-        string temp = "";
-        while(r < haystack.size()){
-            temp += haystack[r];
-            if(temp.size() > needle.size()){
-                temp.erase(temp.begin());
-                l++;
-            }
-            if(temp == needle) return l;
-            r++;
-        }
-        return -1;
+        // size_t index = haystack.find(needle);
+        // if (index == string::npos) {
+        //     return -1;
+        // }
+        // return index;
+        auto it  = search(haystack.begin(), haystack.end(), needle.begin(), needle.end());
+        if(it != haystack.end()) return it - haystack.begin();
+        else return -1;
     }
 };
