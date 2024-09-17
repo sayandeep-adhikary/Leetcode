52db9a010b764d88c832f12d1e80999d47ef6fa0
SAYANDEEP ADHIKARY <sayandeepadhikary.it2021@nsec.ac.in>
2024-09-17 21:09:33 +0530

    Time: 8 ms (33.75%), Space: 12.1 MB (23.28%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int countSegments(string s) {
-        stringstream ss(s);
-        string word;
-        int count = 0;
-        while(ss >> word) count++;
-        return count;
+        // stringstream ss(s);
+        // string word;
+        // int count = 0;
+        // while(ss >> word) count++;
+        // return count;
+        return regex_replace(regex_replace(s, regex("\\S+"), "x"), regex(" "), "").size();
     }
 };
